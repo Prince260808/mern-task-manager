bfb8869f9134ed2aa3645ab8d1ec830e93852dc1
Prince260808 <princewebdev8986@gmail.com>
2026-02-02 09:07:34 +0530

    ready for deploy

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://mern-task-manager-1-lu3w.onrender.com", 
-    headers: {
+  baseURL: "https://mern-task-manager-1-lu3w.onrender.com/api",
+  headers: {
     "Content-Type": "application/json",
   },
 });
